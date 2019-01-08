bb03dedc4766745869f38433e272072a1eebc22b
Valentin Courgeau <valentin.courgeau@free.fr>
2019-01-08 11:54:26 +0000

    Add package loader file

---
+++ R/utils/packages_loader.R
@@ -0,0 +1,28 @@
+
+pckgs_to_load <- c(
+  "igraph",
+  "Rcpp",
+  "stats",
+  "prophet",
+  "statnet",
+  "intergraph",
+  "visNetwork",
+  "magrittr",
+  "mlVAR",
+  "graphicalVAR",
+  "magrittr",
+  "RTransferEntropy",
+  "EntropyEstimation",
+  "CompQuadForm",
+  "JumpTest",
+  "lintr"
+)
+
+check.packages <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg))
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+check.packages(pckgs_to_load)
