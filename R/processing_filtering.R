e80a646a1397e34ed2b9f3fa80c415f502d837fe
Valentin Courgeau <valentin.courgeau@free.fr>
2019-01-08 15:19:14 +0000

    Add time increments matrix and data threshold filtering

--- R/processing_filtering.R
+++ R/processing_filtering.R
@@ -43,5 +43,71 @@ StdTopo <- function(nw_topo){
   return(nw_topo/std_coeff)
 }
 
-# Example
+#' Filters data with respect to given thresholds.
+#' Multidimensional version.
+#' @param data Matrix of data to be filtered.
+#' @param thresholds Continuous threshold for increments.
+#' @param diff_values Boolean to return differenciated values.
+#' @param one_d Boolean it flag if input is one or multidimensional.
+#' @returns Filtered data with respect to increments smaller than
+#'     thresholds. Output as same size as data or shorter by one row
+#'     if diff_values=TRUE.
+#' @examples
+#' data <- c(1,2,5,
+#'           1,3,6,
+#'           1,4,7)
+#' data <- t(matrix(data, ncol=3))
+#' DataFiltering(data, c(2,3,6))
+#' DataFiltering(data, c(2,3,6), diff = T)
+DataFiltering <- function(data, thresholds, diff_values=F, one_d=F){
+  # Shall we have the first or second point when diff_values=F?
+  filtered_data <- diff(data)
+
+  if(one_d){
+    N <- length(data)
+    indicator_data <- (abs(filtered_data) <= thresholds)
+    if(diff_values){
+      return(filtered_data * indicator_data)
+    }else{
+      return(data[-N,] * indicator_data)
+    }
+  }else{
+    N <- length(data[,1])
+    under_thres <- t(apply(abs(filtered_data),
+                           MARGIN = 1, '<=', thresholds))
+    if(diff_values){
+      return(filtered_data*under_thres)
+    }else{
+      return(data[-N,] * under_thres)
+    }
+  }
+}
+
+#' Creates a time increments matrix with the correct
+#' number of columns.
+#'
+#' @param times vector of timestamps.
+#' @param ncol number of columns, i.e. dimensions.
+#' @returns Matrix of time increments with ncol columns.
+#' @examples
+#' times <- c(1,2,4,7,11)
+#' TimeMatrix(times, ncol = 2)
+TimeMatrix <- function(times, ncol){
+  if(ncol <= 0){
+    stop('ncol should be positive.')
+  }
+  # creating increments
+  times <- diff(times)
+
+  if(one_d == 1){
+    return(times)
+  }else{
+    return(t(matrix(rep(times, each=ncol), nrow=ncol)))
+  }
+}
+
+
+
+
+
 
